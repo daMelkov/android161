cb1e89ff95c84d46cbb94b8c075fde65ead5d421
Melkov Denis <d.a.melkov@gmail.com>
2020-09-09 14:44:04 +0300

    add geo intent

--- app/src/main/java/com/astra/melkovhw161/MainActivity.java
+++ app/src/main/java/com/astra/melkovhw161/MainActivity.java
@@ -2,7 +2,14 @@ package com.astra.melkovhw161;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        /* EditText 'Search' */
+        final EditText edtSearch = findViewById(R.id.edtSearch);
+
+        /* Button 'Search' */
+        Button btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String request = edtSearch.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                if(IsCoordinates(request)) {
+                    /* Coordinate request, eg: "55.704968, 37.625206" */
+                    intent.setData(Uri.parse("geo:" + request));
+                } else {
+                    /* Address request, eg: "Roma" */
+                    intent.setData(Uri.parse("geo:?q=" + request));
+                }
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    /* check: request is coordinates */
+    private boolean IsCoordinates(String request) {
+        return !Pattern.compile("[a-zA-Z]").matcher(request).find();
     }
 }
